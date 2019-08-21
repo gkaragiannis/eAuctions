5e3ce5ab6c655c2fc62c4c5d6f272ba296fc5603
gkarag <gkarag@forthnet.gr>
2019-08-21 03:58:08 +0300

    check internet connectivity

--- app/src/main/java/com/dev/e_auctions/MainActivity.java
+++ app/src/main/java/com/dev/e_auctions/MainActivity.java
@@ -1,16 +1,21 @@
 package com.dev.e_auctions;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignUp,btnSignIn,btnGuest;
     TextView txtSlogan;
+    private ConnectivityManager connectivityManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
             }
         });
+
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo==null || !networkInfo.isConnected())
+            Toast.makeText(MainActivity.this, "Please connect to the internet", Toast.LENGTH_LONG).show();
     }
 }
