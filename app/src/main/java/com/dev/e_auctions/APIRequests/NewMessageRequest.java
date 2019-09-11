a27b38bae66a6c7f04209b07538fa04fff39cfd7
gkarag <gkarag@forthnet.gr>
2019-09-11 23:09:29 +0300

    @ Auction Activity * rateUser * sendMessage

--- app/src/main/java/com/dev/e_auctions/APIRequests/NewMessageRequest.java
+++ app/src/main/java/com/dev/e_auctions/APIRequests/NewMessageRequest.java
@@ -3,7 +3,7 @@ package com.dev.e_auctions.APIRequests;
 import com.dev.e_auctions.APIResponses.GeneralResponse;
 import com.dev.e_auctions.Model.Auction;
 
-public class NewMessageRequest extends GeneralResponse {
+public class NewMessageRequest{
     private String token;
     private Auction auction;
     private String subject;
