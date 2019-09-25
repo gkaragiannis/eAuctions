a0b7272f773f3aadd4962f56fc6d0811a583f9af
Dimitris Nikolidakis <dnikolidakis@omilia.com>
2019-09-25 14:00:16 +0300

    getUnsafeOkHttpClientUploadImage

--- app/src/main/java/com/dev/e_auctions/Client/UploadImageClient.java
+++ app/src/main/java/com/dev/e_auctions/Client/UploadImageClient.java
@@ -3,6 +3,14 @@ package com.dev.e_auctions.Client;
 import android.util.Log;
 
 import java.io.File;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -17,14 +25,11 @@ import static com.dev.e_auctions.Client.RestClient.getUnsafeOkHttpClient;
 public class UploadImageClient {
 
 
-    private static OkHttpClient client = getUnsafeOkHttpClient();
+    private static OkHttpClient client = getUnsafeOkHttpClientUploadImage();
 
 
     public static void uploadImage(String token, String auctionId, File file) {
 
-//        final Uri imageUri = data.getData();
-//
-//        final File file = new File(getPath(this, imageUri));
         Log.d("Auction ", "before the body builder ");
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
@@ -52,6 +57,49 @@ public class UploadImageClient {
             Log.d("Auction ", "Not successful upload");
             e.printStackTrace();
         }
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClientUploadImage() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
